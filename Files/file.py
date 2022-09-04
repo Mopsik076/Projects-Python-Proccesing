168ed76d989635f2d7cf4c9f32ea77dea8b9484e
DmitriyDruzhinin <d.druzhinin07@gmail.com>
2022-09-04 21:18:26 +0700

    Registration and Authorization

--- Files/file.py
+++ Files/file.py
@@ -1,1 +1,17 @@
-print('hello')
+start = input('Вход или Регистрация?\n')
+
+if start == 'Регистрация':
+    login = input('Login: ')
+    password = input('Password: ')
+    with open('Files/database.txt', 'w') as f:
+        f.write(login + ' ' + password)
+
+if start == 'Вход':
+    login = input('Login: ')
+    password = input('Password: ')
+    with open('Files/database.txt', 'r') as f:
+        string = ''
+        while 
+            string = f.readline()
+            string.split(' ')
+        
