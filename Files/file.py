474845df4bf39852832bc1b1ef5f447be78c1d5f
DmitriyDruzhinin <d.druzhinin07@gmail.com>
2022-09-18 21:14:54 +0700

    v2

--- Files/file.py
+++ Files/file.py
@@ -1,17 +1,28 @@
-start = input('Вход или Регистрация?\n')
+while True:
+    start = input('Sign in or Sign up?\n')
 
-if start == 'Регистрация':
-    login = input('Login: ')
-    password = input('Password: ')
-    with open('Files/database.txt', 'w') as f:
-        f.write(login + ' ' + password)
+    if start == 'Sign up':
+        login = input('Login: ')
+        password = input('Password: ')
+        with open('C:/Users/Креайтивика/Desktop/Projects Python Proccesing/Files/database.txt', 'w') as f:
+            f.write(login + '\n' + password)
 
-if start == 'Вход':
-    login = input('Login: ')
-    password = input('Password: ')
-    with open('Files/database.txt', 'r') as f:
-        string = ''
-        while 
-            string = f.readline()
-            string.split(' ')
-        
+    if start == 'Sign in':
+        flag = 1
+        login = input('Login: ').rstrip()
+        password = input('Password: ').rstrip()
+        with open('C:/Users/Креайтивика/Desktop/Projects Python Proccesing/Files/database.txt', 'r') as f:
+
+            doc = f.read()
+            strings = doc.split('\n')
+            for k in strings:
+                tmp = k.split(' ')
+                print(tmp)
+                if tmp[0] == login and tmp[1] == password:
+                    flag = 0
+        if flag == 1:
+            print('Not correct')
+        else:
+            print('amazing')
+
+
