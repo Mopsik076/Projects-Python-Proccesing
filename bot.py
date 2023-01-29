5b9911270457dfe683630dc798f534ed14ae032e
Mopsik076 <97390134+Mopsik076@users.noreply.github.com>
2023-01-29 21:10:42 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
+import aiogram.utils.markdown as fmt
+import aiohttp
+from aiogram.utils.emoji import emojize
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -11,19 +14,28 @@ dp = Dispatcher()
 
 @dp.message(commands=["hello"])
 async def cmd_hello(message: types.Message):
-    await message.answer("Hello!")
+    await message.answer(
+        fmt.text(
+            fmt.text(fmt.hbold('Hello')),
+            fmt.text('May name is',fmt.hunderline('Oleg')),
+            sep='\n'),
+        parse_mode='HTML')
 
-@dp.message(commands=['you_here'])
-async def cmd_you_here(message: types.Message):
-    await message.reply('miss me?')
+@dp.message(text='hello')
+async def hello(message: types.Message):
+    await message.answer(fmt.text(fmt.hbold('Hello user')), parse_mode='HTML')
 
-@dp.message(commands=['name'])
-async def cmd_name(message: types.Message):
-    await message.answer('Oleg')
-    
-@dp.message(commands=['you_stupid'])
-async def cmd_you_stupid(message: types.Message):
-    await message.answer('no you!')
+@dp.message_handler(content_types=[types.ContentType.ANIMATION])
+async def reply_gif(message: types.Message):
+    await message.reply_animation(message.animation.file_id)
+
+@dp.message_handler(content_types=[types.ContentType.STICKER])
+async def reply_gif(message: types.Message):
+    await message.reply_animation(message.sticker.file_id)
+
+@dp.message_handler(content_types=[types.ContentType.ANY])
+async def reply_gif(message: types.Message):
+    await message.reply_animation(message.anyany.file_id)
 
 async def main():
     await dp.start_polling(bot)
